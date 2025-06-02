828ad422acfb28d51c337a4ad2b4365a76526f69
benjamin-heasly <benjamin.heasly@gmail.com>
2025-06-02 15:37:44 +0000

    Write output to /results dir.

--- code/hello.py
+++ code/hello.py
@@ -5,6 +5,6 @@ message = os.environ["MESSAGE"]
 print("My message is:")
 print(message)
 
-output_file = "/data/hello.txt"
+output_file = "/results/hello.txt"
 with open(output_file, 'w') as f:
     f.write(message)
